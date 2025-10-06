7be9d52c461e329c0ab84acc1a219c8fb5c8709b
HarshvardhanR <rharsh989878@gmail.com>
2025-10-06 02:13:48 -0400

    added 3d portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,256 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Serverless Blog Platform",
+    description:
+      "Facilitates purchases from international websites like Amazon and eBay, allowing customers to shop from these sites and have products delivered domestically.",
+    subDescription: [
+      "Built a scalable application with ASP.NET Core MVC, integrating global platforms like Amazon for domestic delivery.",
+      "Implemented secure authentication and database management using ASP.NET Core Identity and Entity Framework Core.",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "Added payment systems, localization, and product filtering for functionality improvements.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/skyposts.jpeg",
+    tags: [
+      {
+        id: 1,
+        name: "C#",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 2,
+        name: ".Net",
+        path: "/assets/logos/dotnet.svg",
+      },
+      {
+        id: 3,
+        name: "Ef Core",
+        path: "/assets/logos/efcore.png",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Real-Time Chat Application",
+    description:
+      "A secure authentication and authorization system using Auth0 for seamless user management.",
+    subDescription: [
+      "Integrated Auth0 for authentication, supporting OAuth, JWT, and multi-factor authentication.",
+      "Implemented role-based access control (RBAC) for fine-grained user permissions.",
+      "Developed a React-based frontend with Tailwind CSS for a sleek user experience.",
+      "Connected to a secure SQLite database for user data storage.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/chatApp.webp",
+    tags: [
+      {
+        id: 1,
+        name: "Auth0",
+        path: "/assets/logos/auth0.svg",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "SQLite",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "HTTP-Based Database Management System",
+    description:
+      "A modern, interactive web application built with Blazor WebAssembly and .NET Core.",
+    subDescription: [
+      "Developed a fully interactive Single Page Application (SPA) using Blazor WebAssembly.",
+      "Implemented API interactions using .NET Core for a robust backend.",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
+      "Integrated SQLite for efficient client-side database storage.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/http.webp",
+    tags: [
+      {
+        id: 1,
+        name: "Blazor",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: ".NET Core",
+        path: "/assets/logos/dotnetcore.svg",
+      },
+      {
+        id: 3,
+        name: "SQLite",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "World War III - A CLI-Based Game",
+    description:
+      "A lightweight C++ game engine designed for 2D and 3D game development.",
+    subDescription: [
+      "Built a powerful rendering engine using OpenGL and C++.",
+      "Developed a physics engine with collision detection and particle effects.",
+      "Implemented a scripting system for easy game customization.",
+      "Optimized performance with multi-threading and efficient memory management.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/WW3.png",
+    tags: [
+      {
+        id: 1,
+        name: "C++",
+        path: "/assets/logos/cplusplus.svg",
+      },
+      {
+        id: 2,
+        name: "C#",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 3,
+        name: "Git",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 4,
+        name: "Microsoft",
+        path: "/assets/logos/microsoft.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Flight Delay Prediction",
+    description:
+      "A fully customizable WordPress theme optimized for performance and SEO.",
+    subDescription: [
+      "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+      "Integrated Tailwind CSS for modern styling and UI enhancements.",
+      "Optimized SEO and page speed using Vite.js for fast builds.",
+      "Implemented custom widgets and plugin compatibility for extended functionality.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/fd.png",
+    tags: [
+      {
+        id: 1,
+        name: "WordPress",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 2,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "Vite.js",
+        path: "/assets/logos/vitejs.svg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "EEG Decoding",
+    description:
+      "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+    subDescription: [
+      "Built using Blazor WebAssembly for a seamless SPA experience.",
+      "Implemented video streaming with Azure Media Services.",
+      "Added a quiz system with dynamic question generation and real-time grading.",
+      "Integrated Stripe API for secure payment processing.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/eeg.png",
+    tags: [
+      {
+        id: 1,
+        name: "Blazor",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: "Azure",
+        path: "/assets/logos/azure.svg",
+      },
+      {
+        id: 3,
+        name: "Stripe",
+        path: "/assets/logos/stripe.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/harshvardhansingh-rao-a63929222/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Github",
+    href: "https://github.com/HarshvardhanR",
+    icon: "/assets/socials/github.svg",
+  },
+  {
+    name: "Leetcode",
+    href: "https://leetcode.com/u/r_harshv/",
+    icon: "/assets/socials/leetcode.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Full Stack Developer Intern",
+    job: "Media Platforms",
+    date: "June 2021 - November 2021",
+    contents: [
+      "Optimized front-end performance, improving page load speed by 25% through restructuring HTML/CSS, removing render-blocking scripts, and implementing lazy loading strategies.",
+      "Increased backend query performance by 30% by redesigning filtering logic and introducing targeted MongoDB indexing, reducing response times on high-traffic endpoints",
+      "Redesigned and modularized React UI components, streamlining navigation workflows and enhancing filter precision, leading to a 20% increase in user engagement.",
+      "Reduced API response latency by 15% by refactoring Django views and adopting asynchronous request handling, improving system scalability and user experience.",
+    ],
+  }
+];
